a6da67e631c9c6a4b1cdd581937ba8b15909b252
Romano Livi <romano_livi@outlook.com>
2020-12-12 21:24:48 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,8 +3,9 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length 
-   if yield(collection[i]) == true
+    if yield(collection[i]) == true
      return yield(collection[i])
+    end
   i += 1 
   end
 end
